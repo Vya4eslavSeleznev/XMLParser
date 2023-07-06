0ec18b66f564d62eb5f247cc5010725e88ad593f
Vya4eslav111 <vya4eslav.seleznev@gmail.com>
2023-07-06 16:16:44 +0300

    added template in xml

--- src/main/java/com/xml/parser/service/impl/TemplateProcessService.java
+++ src/main/java/com/xml/parser/service/impl/TemplateProcessService.java
@@ -1,12 +1,15 @@
 package com.xml.parser.service.impl;
 
+import com.xml.parser.model.EmployeeModel;
 import com.xml.parser.service.ParserService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -21,7 +24,22 @@ public class TemplateProcessService {
         Context context = new Context();
         //context.setVariable("map", map);
         //map.put("theme", "TEST");
-        context.setVariable("description", "TEST");
+
+
+
+//        List<EmployeeModel> list = new ArrayList<>();
+//
+//        list.add(new EmployeeModel("q", "w", "e", "r"));
+//        list.add(new EmployeeModel("t", "y", "u", "i"));
+//        list.add(new EmployeeModel("o", "p", "]", "["));
+
+
+
+        List<String> list = new ArrayList<>();
+        list.add("HELLO");
+        list.add("WORLD");
+
+        context.setVariable("messages", list);
 
         String content = springTemplateEngine.process("template",context);
         System.out.println();
